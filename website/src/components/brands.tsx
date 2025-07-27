62f9a47ad95e9d55030d973cd0a725e2c0e6377c
Bruce <97542494+BruceNiemi@users.noreply.github.com>
2025-07-26 22:05:47 -0500

    chore: source logos for the brands

---
+++ website/src/components/brands.tsx
@@ -0,0 +1,88 @@
+const brands = [
+  {
+    name: "Rheem",
+    logo: "/images/brands/rheem.svg",
+  },
+  {
+    name: "Ducane",
+    logo: "/images/brands/ducane.png",
+  },
+  {
+    name: "Samsung",
+    logo: "/images/brands/samsung.png",
+  },
+  {
+    name: "Allied Commercial",
+    logo: "/images/brands/allied-commercial.png",
+  },
+  {
+    name: "Fresh-AirUV",
+    logo: "/images/brands/fresh-air-uv.png",
+  },
+  {
+    name: "AO Smith",
+    logo: "/images/brands/ao-smith.png",
+  },
+  {
+    name: "Comfort-Aire",
+    logo: "/images/brands/comfort-aire.png",
+  },
+  {
+    name: "Daikin",
+    logo: "/images/brands/daikin.png",
+  },
+  {
+    name: "Danfoss",
+    logo: "/images/brands/danfoss.svg",
+  },
+  {
+    name: "Electrohome",
+    logo: "/images/brands/electrohome.png",
+  },
+  {
+    name: "Engineered Air",
+    logo: "/images/brands/engineered-air.png",
+  },
+  {
+    name: "G.E",
+    logo: "/images/brands/ge.png",
+  },
+  {
+    name: "Honeywell",
+    logo: "/images/brands/honeywell.png",
+  },
+  {
+    name: "Johnson Controls",
+    logo: "/images/brands/johnson-controls.png",
+  },
+  {
+    name: "KeepRite",
+    logo: "/images/brands/keeprite.png",
+  },
+  {
+    name: "Lennox",
+    logo: "/images/brands/lennox.png",
+  },
+  {
+    name: "Liebert",
+    logo: "/images/brands/liebert.png",
+  },
+  {
+    name: "Modine",
+    logo: "/images/brands/modine.png",
+  },
+  {
+    name: "Nailor",
+    logo: "/images/brands/nailor.png",
+  },
+  {
+    name: "Payne",
+    logo: "/images/brands/payne.jpg",
+  },
+  {
+    name: "Reznor",
+    logo: "/images/brands/reznor.png",
+  },
+];
+
+export default function Brands() {}
